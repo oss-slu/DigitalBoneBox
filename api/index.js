4a63c90699b3aa7cde046d0e054b0ff10e1f7a25
Remex_9 <ramedan.ahmed@slu.edu>
2026-09-11 15:50:12 -0500

    Fix Vercel function crash: root rate-limit dep and data includes

--- api/index.js
+++ api/index.js
@@ -1,3 +1,6 @@
-import { app } from "../boneset-api/server.js";
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
+const { app } = require("../boneset-api/server.js");
 
 export default app;
